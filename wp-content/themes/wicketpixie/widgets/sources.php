31d396a720ec34ad3a85554de69c782650f184e9
Eddie Ringle <eddie@eringle.net>
2009-01-12 06:20:51 -0500

    1.0.2 upload
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

---
+++ wp-content/themes/wicketpixie/widgets/sources.php
@@ -0,0 +1,7 @@
+<?php
+			function wicketpixie_pownce() {
+				include( ABSPATH . 'wp-content/themes/wicketpixie/widgets/pownce.php');
+			}
+			function wicketpixie_twitter() {
+				include( ABSPATH . 'wp-content/themes/wicketpixie/widgets/twitter.php');
+			} ?>
