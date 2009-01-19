f5edc45d0eb827ababb348f14d8e93280c061443
Eddie Ringle <eddie@eringle.net>
2009-01-18 21:05:34 -0500

    URLs in the Status updates box are now hyperlinked.
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- wicketpixie/app/update.php
+++ wicketpixie/app/update.php
@@ -32,8 +32,11 @@ class SourceUpdate
                     $update[]['link']= $entry->get_permalink();
                     $update[]['date']= strtotime( substr( $entry->get_date(), 0, 25 ) );
                 }
-                
-                $return= array_slice( $update, 0, 5);			
+				
+                $return= array_slice( $update, 0, 5);	
+
+				$return[1]['title'] = preg_replace('((?:\S)+://\S+[[:alnum:]]/?)', '<a href="\0">\0</a>', $return[1]['title']);
+				
                 return substr($return[1]['title'], 0, 150) . ' &mdash; <a href="' . $return[2]['link'] . '" title="">' . date( 'g:ia', $return[3]['date'] ) . '</a>';
         } else {
             return 'No source configured.';
